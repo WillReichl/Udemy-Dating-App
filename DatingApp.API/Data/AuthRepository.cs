8707aaf0483496c3597091044c21a018c4025259
Will Reichl <willreichl@gmail.com>
2019-02-11 06:40:56 -0500

    S3L26 - Creating the Concrete Auth Repo and Async Register Method

---
+++ DatingApp.API/Data/AuthRepository.cs
@@ -0,0 +1,48 @@
+using System;
+using System.Threading.Tasks;
+using DatingApp.API.Models;
+
+namespace DatingApp.API.Data
+{
+    public class AuthRepository : IAuthRepository
+    {
+        private readonly DataContext _dataContext;
+
+        public AuthRepository(DataContext dataContext)
+        {
+            this._dataContext = dataContext;
+        }
+
+        public Task<User> Login(string username, string password)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public async Task<User> RegisterAsync(User user, string password)
+        {
+            byte[] passwordHash, passwordSalt;
+            CreatePasswordHash(password, out passwordHash, out passwordSalt);
+
+            user.PasswordHash = passwordHash;
+            user.PasswordSalt = passwordSalt;
+
+            await _dataContext.Users.AddAsync(user);
+            await _dataContext.SaveChangesAsync();
+
+            return user;
+        }
+
+        private void CreatePasswordHash(string password, out byte[] passwordHash, out byte[] passwordSalt)
+        {
+            using (var hmac = new System.Security.Cryptography.HMACSHA512()) {
+                passwordHash = hmac.Key;
+                passwordSalt = hmac.ComputeHash(System.Text.Encoding.UTF8.GetBytes(password));
+            }
+        }
+
+        public Task<bool> UserExists(string username)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
--- DatingApp.API/Data/IAuthRepository.cs
+++ DatingApp.API/Data/IAuthRepository.cs
@@ -5,7 +5,7 @@ namespace DatingApp.API.Data
 {
     public interface IAuthRepository
     {
-         Task<User> Register(User user, string password);
+         Task<User> RegisterAsync(User user, string password);
          Task<User> Login (string username, string password);
          Task<bool> UserExists (string username);
     }
