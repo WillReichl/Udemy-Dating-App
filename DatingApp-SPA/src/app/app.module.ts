a61a5092b8c0def28fcafcf13f9c21b0bc51079c
Will Reichl <willreichl@gmail.com>
2019-02-16 15:22:28 -0500

    Parent to Child Input Variable & ngFor Directive

--- DatingApp-SPA/src/app/app.module.ts
+++ DatingApp-SPA/src/app/app.module.ts
@@ -4,7 +4,6 @@ import { HttpClientModule } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
-import { ValueComponent } from './value/value.component';
 import { NavComponent } from './nav/nav.component';
 import { AuthService } from './_services/auth.service';
 import { HomeComponent } from './home/home.component';
@@ -13,8 +12,6 @@ import { RegisterComponent } from './register/register.component';
 @NgModule({
    declarations: [
       AppComponent,
-      ValueComponent,
-      ValueComponent,
       NavComponent,
       HomeComponent,
       RegisterComponent
