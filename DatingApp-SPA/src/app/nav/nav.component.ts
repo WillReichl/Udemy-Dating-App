b686d70dc53c85a1a85920e5ebbb79c876e21e38
Will Reichl <willreichl@gmail.com>
2019-02-18 06:46:36 -0500

    Using Angular JWT library to improve token handling

--- DatingApp-SPA/src/app/nav/nav.component.ts
+++ DatingApp-SPA/src/app/nav/nav.component.ts
@@ -26,8 +26,7 @@ export class NavComponent implements OnInit {
   }
 
   loggedIn() {
-    const token = localStorage.getItem('token');
-    return !!token;
+    this.authService.loggedIn();
   }
 
   logout() {
