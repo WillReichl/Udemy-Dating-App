002ee0aa9880915c109ea51fc8cd9ffd5f416528
Will Reichl <willreichl@gmail.com>
2019-02-24 13:14:57 -0500

    S8 - Creating new repository for API - Repository Pattern

---
+++ DatingApp.API/Data/DatingRepository.cs
@@ -0,0 +1,44 @@
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using DatingApp.API.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace DatingApp.API.Data
+{
+    public class DatingRepository : IDatingRepository
+    {
+        private readonly DataContext _dbContext;
+
+        public DatingRepository(DataContext dbContext)
+        {
+            this._dbContext = dbContext;
+        }
+
+        public void Add<T>(T entity) where T : class
+        {
+            _dbContext.Add(entity);
+        }
+
+        public void Delete<T>(T entity) where T : class
+        {
+            _dbContext.Remove(entity);
+        }
+
+        public async Task<User> GetUser(int id)
+        {
+            var user = await _dbContext.Users.Include(u => u.Photos).FirstOrDefaultAsync(u => u.Id == id);
+            return user;
+        }
+
+        public async Task<IEnumerable<User>> GetUsers()
+        {
+            var user = await _dbContext.Users.Include(u => u.Photos).ToListAsync();
+            return user;
+        }
+
+        public async Task<bool> SaveAll()
+        {
+            return await _dbContext.SaveChangesAsync() > 0;
+        }
+    }
+}
