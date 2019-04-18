2c18f687e4bfe3256ef4fa14b9ad09cedd6ed27f
Will Reichl <willreichl@gmail.com>
2019-04-18 07:12:11 -0400

    S14 - Setting up the Paging Helper Classes

--- DatingApp.API/Helpers/Extensions.cs
+++ DatingApp.API/Helpers/Extensions.cs
@@ -1,17 +1,27 @@
 using System;
 using Microsoft.AspNetCore.Http;
+using Newtonsoft.Json;
 
 namespace DatingApp.API.Helpers
 {
     public static class Extensions
     {
-        public static void AddApplicationError(this HttpResponse response, string message) {
+        public static void AddApplicationError(this HttpResponse response, string message)
+        {
             response.Headers.Add("Application-Error", message);
             response.Headers.Add("Access-Control-Expose-Headers", "Application-Error");
             response.Headers.Add("Access-Control-Allow-Origin", "*");
         }
 
-        public static int CalculateAge(this DateTime dateOfBirth) 
+        public static void AddPagination(this HttpResponse response, int currentPage,
+            int itemsPerPage, int totalItems, int totalPages)
+        {
+            var paginationHeader = new PaginationHeader(currentPage, itemsPerPage, totalItems, totalPages);
+            response.Headers.Add("Pagination", JsonConvert.SerializeObject(paginationHeader));
+            response.Headers.Add("Access-Control-Expose-Headers", "Pagination"); // Prevent CORS error
+        }
+
+        public static int CalculateAge(this DateTime dateOfBirth)
         {
             var age = DateTime.Today.Year - dateOfBirth.Year;
             if (dateOfBirth.AddYears(age) > DateTime.Today) // haven't had birthday this year yet
